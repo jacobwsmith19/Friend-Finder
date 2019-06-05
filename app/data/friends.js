716a833967c816ac114bc0902eb1739b0160b30a
Jake Smith <jacobwsmith19@gmail.com>
2019-06-05 13:00:39 -0500

    Completed javascript files

---
+++ app/data/friends.js
@@ -0,0 +1,39 @@
+var friends = [
+    {
+      name: "Amy Adams",
+      photo:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/1/12/Amy_Adams_UK_Nocturnal_Animals_Premiere_%28cropped%29.jpg/220px-Amy_Adams_UK_Nocturnal_Animals_Premiere_%28cropped%29.jpg",
+      scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 4]
+    },
+    {
+      name: "Bob Barker",
+      photo:
+        "https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cg_face%2Cq_auto:good%2Cw_300/MTE4MDAzNDEwNzQ5MTk1Nzkw/bob-barker-9542237-1-402.jpg",
+      scores: [4, 2, 5, 1, 3, 2, 2, 1, 3, 2]
+    },
+    {
+      name: "Chris Carter",
+      photo:
+        "https://ethicsalarms.files.wordpress.com/2015/08/chris-carter.jpg",
+      scores: [5, 2, 2, 2, 4, 1, 3, 2, 5, 5]
+    },
+    {
+      name: "Dale Dudley",
+      photo:
+        "https://i.ytimg.com/vi/rdloXFMLQs4/maxresdefault.jpg",
+      scores: [3, 3, 4, 2, 2, 1, 3, 2, 2, 3]
+    },
+    {
+      name: "Ed Eddy",
+      photo:
+        "https://upload.wikimedia.org/wikipedia/en/thumb/c/ce/All_3_eds.png/200px-All_3_eds.png",
+      scores: [4, 3, 4, 1, 5, 2, 5, 3, 1, 4]
+    },
+    {
+      name: "Fred Flinstone",
+      photo:
+        "https://vignette.wikia.nocookie.net/flinstones/images/a/ad/Fred_Flintstone.png/revision/latest?cb=20110919071811",
+      scores: [4, 3, 4, 2, 4, 3, 5, 3, 5, 4]
+    }
+  ];
+  module.exports = friends;
---
+++ server.js
@@ -0,0 +1,20 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+var app = express();
+var port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+app.use(express.static("app/public"));
+
+require("./app/routing/apiRoutes.js")(app);
+require("./app/routing/htmlRoutes.js")(app);
+
+app.listen(PORT, function() {
+	console.log("App is listening on PORT: " + PORT);
+});
